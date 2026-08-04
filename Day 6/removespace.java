3bee060c14a053f9bc48df52ca7c436f4cf2df89
GANESH27B <bommidiganesh27@gmail.com>
2026-08-04 16:18:17 +0530

    upadte

---
+++ Day 6/removespace.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class removespace{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the String: ");
+        String str = sc.nextLine();
+        String chart = "";
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) == ' '){
+                
+            }else{
+                chart +=str.charAt(i);
+            }
+    
+        }
+        System.out.print("Final : " + chart);
+
+        
+    }
+}
---
+++ Day 6/uppertolower.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class uppertolower{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the String: ");
+        String str = sc.nextLine();
+        for(int i=0;i<str.length() ; i++){
+            if(Character.isUpperCase(str.charAt(i))){
+                char ch = Character.toLowerCase(str.charAt(i));
+                System.out.print(ch);
+            }else{
+                char ch = Character.toUpperCase(str.charAt(i));
+                System.out.print(ch);
+            }
+        }
+        
+    }
+}
